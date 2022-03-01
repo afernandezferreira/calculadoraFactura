67e814919e1d2f2e3301d5dd4d5c1b0f7826e5d2
Alex <75092742+afernandezferreira@users.noreply.github.com>
2022-03-01 17:07:14 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,36 @@
+function calculoFactura() {
+        let total = document.getElementById("total").value;
+        let diasFactura = document.getElementById("diasFactura").value;
+
+        let pers1 = document.getElementById("pers1").value;
+        let pers2 = document.getElementById("pers2").value;
+        let pers3 = document.getElementById("pers3").value;
+        let pers4 = document.getElementById("pers4").value;
+
+        let dias1 = document.getElementById("dias1").value;
+        let dias2 = document.getElementById("dias2").value;
+        let dias3 = document.getElementById("dias3").value;
+        let dias4 = document.getElementById("dias4").value;
+
+        let diasxpers = parseInt(dias1) + parseInt(dias2) + parseInt(dias3) + parseInt(dias4);
+        let ppd = (total / diasxpers);  //Precio x persona día
+
+        let ppdRound = Math.round(((ppd) + Number.EPSILON) * 100) / 100;
+
+        p1 = Math.round(((ppd * dias1) + Number.EPSILON) * 100) / 100;
+        p2 = Math.round(((ppd * dias2) + Number.EPSILON) * 100) / 100;
+        p3 = Math.round(((ppd * dias3) + Number.EPSILON) * 100) / 100;
+        p4 = Math.round(((ppd * dias4) + Number.EPSILON) * 100) / 100;
+
+        tot = Math.round((((ppd * dias1) + (ppd * dias2) + (ppd * dias3) + (ppd * dias4)) + Number.EPSILON) * 100) / 100;
+
+        let text;
+        text = 'precio/pers/dia = ' + ppdRound + '€<br><br>' +
+                pers1 + ' ' + dias1 + ' dias -->' + p1 + '€<br>' +
+                pers2 + ' ' + dias2 + ' dias -->' + p2 + '€<br>' +
+                pers3 + ' ' + dias3 + ' dias -->' + p3 + '€<br>' +
+                pers4 + ' ' + dias4 + ' dias -->' + p4 + '€<br><br>' +
+                'Total factura: '+tot + '€';
+
+        document.getElementById("demo").innerHTML = text;
+}
